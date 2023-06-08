db86ed383e8f6eec7624f5c0e5fbf08b9a642336
Emily-Bautista <112963205+Emily-Bautista@users.noreply.github.com>
2023-06-08 14:35:26 -0600

    Formulario Actualizado 3

---
+++ registrar.php
@@ -0,0 +1,30 @@
+<?php 
+
+include("con_db.php");
+
+if (isset($_POST['register'])) {
+    if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
+	    $name = trim($_POST['name']);
+	    $email = trim($_POST['email']);
+	    $fechareg = date("d/m/y");
+		$escuela = trim($_POST['escuela']);
+		$numero = trim($_POST['numero']);
+	    $consulta = "INSERT INTO datos(nombre, email, fecha_reg, escuela, numero) VALUES ('$name','$email','$fechareg','$escuela','$numero')";
+	    $resultado = mysqli_query($conex,$consulta);
+	    if ($resultado) {
+	    	?> 
+	    	<h3 class="ok">¡Te has postulado , bien por ti!</h3>
+           <?php
+	    } else {
+	    	?> 
+	    	<h3 class="bad">¡Ups ha ocurrido un errorsote!</h3>
+           <?php
+	    }
+    }   else {
+	    	?> 
+	    	<h3 class="bad">Compos imcompletos ¡Por favor complete los campos!</h3>
+           <?php
+    }
+}
+
+?>
